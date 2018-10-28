223e1c993ae4cfdeffdc85fe656c7adf368144aa
Wind <zkjcol@126.com>
2018-10-28 14:30:19 +0800

    Programme

--- port_p250/main.cpp
+++ port_p250/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+class ship
+{
+public:
+    long time;
+    int passenger;
+    vector<int> nati;
+    void in()
+    {
+        cin>>time>>passenger;
+        for(int i=0;i<passenger;i++)
+        {
+            int tmp;
+            cin>>tmp;
+            nati.push_back(tmp);
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<ship> shp;
+    int nos;
+    cin>>nos;
+    vector<int> nation;
+    for(int i=0;i<nos;i++)
+    {
+        ship tmp;
+        tmp.in();
+        shp.push_back(tmp);
+    }
+    for(int i=0;i<shp.size();i++)
+    {
+        set<int> s;
+        for(int j=0;j<shp.size();j++)
+        {
+            if(shp[j].time>shp[i].time-86400&&shp[j].time<=shp[i].time)
+            {
+                for(int k=0;k<shp[j].nati.size();k++)
+                {
+                    int a=(shp[j].nati)[k];
+                    s.insert(a);
+                }
+            }
+        }
+        cout<<s.size()<<endl;
+    }
     return 0;
 }
