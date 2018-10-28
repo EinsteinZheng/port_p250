8f07bd188a93e3abfec5aae11b1ae31b1bf03d83
Wind <zkjcol@126.com>
2018-10-28 13:53:26 +0800

    Initial Commit

---
+++ port_p250/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  port_p250
+//
+//  Created by Zheng on 10/28/18.
+//  Copyright © 2018 Monkey Coder Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
